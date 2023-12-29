24120be97ca7c4949ee7b78ef850281ea8c152e7
Sima Amini <sima.amini@connect2wow.com>
2023-12-30 00:58:43 +0330

    Create search-box

---
+++ src/components/search-box/search-box.jsx
@@ -0,0 +1,13 @@
+export const SearchBox = () => {
+  return (
+    <div className="join">
+      <input
+        className="input input-bordered join-item"
+        placeholder="نام یا شماره تلفن را وارد کنید"
+      />
+      <button className="btn btn-primary join-item rounded-r-full">
+        جست و جو
+      </button>
+    </div>
+  );
+};
