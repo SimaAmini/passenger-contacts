33d9d9243da6d35324aa24590cb0e875c538ecac
Sima Amini <sima.amini@connect2wow.com>
2024-01-01 17:35:05 +0330

    Fix duplicated freq contacts

--- src/store/freq-store.js
+++ src/store/freq-store.js
@@ -2,7 +2,6 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { useContactStore } from "./contact-store";
 import { sortByKey } from "../utils";
-
 const useFreqContactStore = create(
   persist(
     (set, get) => ({
@@ -27,14 +26,27 @@ export const useFreqContacts = () => {
     const index = contacts.findIndex((contact) => contact.id == contactId);
     contacts[index].lastVisited = Date.now();
 
-    const list = sima([...freqContacts, contacts[index]]);
+    const list = getFirst4Items(
+      addToFreqContacts(freqContacts, contacts[index]),
+    );
+
     setFreqContacts(list);
   };
 
   return { setLastVisited, getFreqContacts };
 };
 
-const sima = (list) => {
+const getFirst4Items = (list) => {
   const sorted = sortByKey(list, "lastVisited");
   return sorted.slice(0, 4);
+};
+
+const addToFreqContacts = (list, selected) => {
+  const index = list.findIndex((item) => item.id === selected.id);
+
+  if (index === -1) return [...list, selected];
+
+  list[index] = selected;
+
+  return list;
 };
